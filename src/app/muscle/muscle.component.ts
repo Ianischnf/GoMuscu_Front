ff0ebdf908aa763225e3b5527b211a9610f10322
Salman-Ali05 <sallukhan0805gmail.com>
2023-11-06 10:43:59 +0100

    Example Get data from Backend api

--- src/app/muscle/muscle.component.ts
+++ src/app/muscle/muscle.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-muscle',
   templateUrl: './muscle.component.html',
-  styleUrls: ['./muscle.component.css']
+  styleUrls: ['./muscle.component.css'],
 })
 export class MuscleComponent {
+  muscles: any[] = [];  // Créez une propriété pour stocker les données
 
+  constructor(private http: HttpClient) { }
+
+  getData() {
+    this.http.get('http://localhost:8000/muscle').subscribe((data: any) => {
+      this.muscles = data.results;  // Stockez les données dans la propriété muscles
+    });
+  }
 }
